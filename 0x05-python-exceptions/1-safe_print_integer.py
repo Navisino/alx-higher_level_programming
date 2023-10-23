4686d3ee5eb04d821ccb2e63b34ce95f5e423718
Kate Navisino <107048843+Navisino@users.noreply.github.com>
2023-10-24 01:26:48 +0300

    1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+
+
 def safe_print_integer(value):
+    """
+    prints a list of anything, but only prints the integers
+    Returns the amount of integers printed
+    """
     try:
         print("{:d}".format(value))
-    except(ValueError, TypeError):
-        return False
-    else:
         return True
+    except:
+        return False
